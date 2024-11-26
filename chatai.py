779f6d69125378553e96e046af66c5b7c1cc8ed5
Alexander Jansson <alexanderjson@hotmail.com>
2024-11-26 21:01:27 +0100

    added token embedding

--- chatai.py
+++ chatai.py
@@ -2,7 +2,18 @@ from gensim.downloader import load
 
 glove_vector_test = load('glove-wiki-gigaword-300')
 
-print(glove_vector_test.most_similar('king'))
-print(glove_vector_test.most_similar('queen'))
 
-print("king: ", glove_vector_test['king'])
+# print("king: ", glove_vector_test['king'])
+
+
+def tokenise_input():
+    user_input = input("Enter a word: ")
+    words = user_input.split()
+    for word in words: 
+        if word in glove_vector_test:
+            print("word: ", glove_vector_test[word])
+        else:
+            print("word not in dictionary")
+
+
+tokenise_input()
